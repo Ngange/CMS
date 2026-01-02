0e7db17ce8be2eeac092f91cdaa3820bf24dfdf2
Ngange <ngangebabu@gmail.com>
2026-01-02 10:31:35 +0000

    fix dev backendUrl link

--- cms-frontend/src/environments/environment.ts
+++ cms-frontend/src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://cms-rrzq.onrender.com/api',
-  // backendUrl: 'https://cms-rrzq.onrender.com',
   apiUrl: 'http://localhost:3000/api',
   backendUrl: 'http://localhost:3000',
   cloudinary: {
