1efdfb03e8ce96b79ee33f59bdb5f62e3016c9ef
Ngange <ngangebabu@gmail.com>
2026-01-02 10:19:13 +0000

    fix local dev build

--- cms-frontend/src/environments/environment.ts
+++ cms-frontend/src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://cms-rrzq.onrender.com/api',
-  backendUrl: 'https://cms-rrzq.onrender.com',
+  // apiUrl: 'https://cms-rrzq.onrender.com/api',
+  // backendUrl: 'https://cms-rrzq.onrender.com',
+  apiUrl: 'http://localhost:3000/api',
+  backendUrl: 'http://localhost:3000',
   cloudinary: {
     cloudName: 'dhnoiitry',
     unsignedUploadPreset: 'cms_unsigned'
